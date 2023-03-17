48d15205acc08f8f36f6091ca51a735ba7f51faf
CindyMisoi <crmisoi2k@gmail.com>
2023-03-17 10:21:59 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get students
+  get '/students', to: 'students#index'
+
+  # get grades
+  get '/students/grades', to: 'students#grades'
+
+  # get student with the highest grade
+  get '/students/highest-grade', to: 'students#highest_grade'
 end
